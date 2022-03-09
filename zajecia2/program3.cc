b0bf5d6215dc00ca46d35175a6fe006a14eea453
Jakub Kozubek <61938161+Jaaccob@users.noreply.github.com>
2022-03-09 12:28:21 +0100

    program3

---
+++ zajecia2/program3.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+namespace przestrzen_nazw{
+	float pi = 3.14;
+}
+
+//using namespace przestrzen_nazw;
+
+int main(){
+//	cout << pi < endl;
+	cout << przestrzen_nazw::pi << endl;
+	return 1;
+}
